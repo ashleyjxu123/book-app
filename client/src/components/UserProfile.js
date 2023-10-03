85383ec3e4546f805850a56c65a6d05be40ebe6c
Natasha Aysseh <nca28@cornell.edu>
2023-10-03 09:29:11 -0400

    new files

---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import {Link} from "react-router-dom";
+// import { useParams } from "react-router";
+import '../styles/UserProfile.css'
+
+
+
+export default function UserProfile() {
+  
+
+}
+
